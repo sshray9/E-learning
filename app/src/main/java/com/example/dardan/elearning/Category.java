f1b857aa8207dd1c46755c7990755138c1bce1c6
shray sharma <sshray9@gmail.com>
2021-01-17 01:29:14 +0530

    E-learning

---
+++ app/src/main/java/com/example/dardan/elearning/Category.java
@@ -0,0 +1,62 @@
+package com.example.dardan.elearning;
+
+import java.util.ArrayList;
+
+
+public class Category {
+    ArrayList<Thing> things;
+    int currentIndex=0;
+    String title;
+    int image;
+    int highscore;
+    int color;
+    int theme;
+    String columnName;
+    int quizImage;
+
+    public Category(String title,int image,int highscore,int color,int theme, String columnName, int quizImage){
+        this.title=title;
+        this.image=image;
+        this.highscore=highscore;
+        this.color=color;
+        this.theme=theme;
+        this.columnName=columnName;
+        this.quizImage=quizImage;
+        this.things=new ArrayList<>();
+    }
+    public void addThing(Thing thing){
+        things.add(thing);
+    }
+
+    public ArrayList<Thing> getListOfThings(){
+        return this.things;
+    }
+
+    public Thing nextThing() {
+        return things.get(++currentIndex);
+    }
+
+    public Thing prevThing() {
+        return things.get(--currentIndex);
+    }
+
+    public Thing currentThing() {
+        return things.get(currentIndex);
+    }
+
+    boolean hasNextThing(){
+        return currentIndex < things.size()-1;
+    }
+
+    boolean hasPrevThing(){
+        return currentIndex > 0;
+    }
+
+    public void goToFirstThing(){
+        currentIndex=0;
+    }
+
+    public void updateHighscore(){
+        this.highscore = Highscores.getHighscore(this.columnName);
+    }
+}
